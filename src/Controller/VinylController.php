9097984a24b01c83799582ad35a1f1f22434c0c8
David Lombera <david.lomberlan@gmail.com>
2023-06-05 16:23:50 -0600

    04 - Wildcard Routes

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,11 +5,25 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use function Symfony\Component\String\u;
+
 class VinylController
 {
     #[Route('/')]
     public function homePage(): Response
     {
         return new Response('Title: Viva la vida');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Gender: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genders';
+        }
+
+        return new Response($title);
     }
 }
